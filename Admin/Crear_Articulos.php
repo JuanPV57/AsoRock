84690af367e7cdc0680622edaf4fa72d54409dad
Juan <juanvelcoc@gmail.com>
2020-08-07 21:52:19 -0500

    Update

---
+++ Admin/Crear_Articulos.php
@@ -0,0 +1,36 @@
+<?php 
+
+    //Conexion a la base de datos
+    include('../ConexionDB/conexion.php');
+
+    if(isset($_POST['guardar'])){
+        $nombreImagen = $_FILES['imagen']['name'];
+        $tamanoImagen = $_FILES['imagen']['size'];
+        
+        if($tamanoImagen<=2000000){
+            $carpetaDestino = $_SERVER['DOCUMENT_ROOT'].'/AsoRock/ImgPublicaciones/Articulos/';
+            move_uploaded_file($_FILES['imagen']['tmp_name'],$carpetaDestino.$nombreImagen);
+        }
+
+        $titulo = $_POST['titulo'];
+        $slug = $_POST['slug'];
+        $fecha = $_POST['fecha'];
+        $autor = $_POST['autor'];
+        $contenido = $_POST['contenido'];
+
+        $query = "INSERT INTO articulos (Titulo, Slug, Autor, Contenido, Imagen, Fecha) values('$titulo', '$slug', '$autor', '$contenido', '$nombreImagen', '$fecha')";
+        $resultado = mysqli_query($conexion,$query);
+
+        if(!$resultado){
+            echo "No se pudo agregar a la base de datos";
+        }
+
+        header("Location: ../Admin/Lista_Crear_Articulo.php");
+
+    }
+
+
+
+
+
+?>
