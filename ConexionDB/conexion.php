066ba5cc83788fb03eb445a4dd09f1ccab818ce3
steven <steven-jocz@hotmail.com>
2020-08-07 19:56:25 -0500

    Actualizado

--- ConexionDB/conexion.php
+++ ConexionDB/conexion.php
@@ -1,15 +1,1 @@
-<?php
 
-    $conexion = mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'AsoRock'
-
-    );
-    if(!$conexion){
-        die("ERROR! No se pudo agregar a la DB");
-    }
-    
-
-?>
